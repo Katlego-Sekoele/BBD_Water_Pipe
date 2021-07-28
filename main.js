b39abf3f1f31c7b3a7f562e7901833ab4e0211ba
Katlego-Sekoele <sekoelekatlego@gmail.com>
2021-07-28 12:27:14 +0200

    Added a function to generate sprites
    Added a function to generate sprites.
    Referenced the waterPassage.js file in index.html.
    edited the grid system.
    added a space at the top of the screen to hold newly generated items.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+var OFFSET = 21.875;
+var CELL_WIDTH = 43.75;
+
 var config = {
     type: Phaser.AUTO,
     width: 743.75,
-    height: 743.75,
+    height: 743.75+CELL_WIDTH,
     physics: {
         default: 'arcade',
         arcade: {
@@ -16,30 +19,37 @@ var config = {
 };
  
 var game = new Phaser.Game(config); 
-var OFFSET = 21.875;
-var CELL_WIDTH = 43.75;
  
 function preload ()
 {
     this.load.image('board', 'assets/grid.png');
+
+    this.load.image('source', 'assets/start.png');
+    this.load.image('end', 'assets/end.png');
+
     this.load.image('pipe', 'assets/pipe.png');
+    this.load.image('bend_left', 'assets/bend_left.png');
+    this.load.image('bend_right', 'assets/bend_right.png');
+    this.load.image('check_pipe', 'assets/checkerpipe_clean.png');
+    this.load.image('double_pipe_dual', 'assets/doublepipe_dual.png');
+    this.load.image('double_pipe_left', 'assets/doublepipe_left.png');
+    this.load.image('double_pipe_right', 'assets/doublepipe_right.png');
+    this.load.image('purifier', 'assets/purify.png');
+    
+
 }
  
 function create ()
 {
  
     grid = create_grid(); // creates a 2D array of 16x16
- 
-    this.add.image(OFFSET, OFFSET, 'board').setOrigin(0,0); //set the origin of the image to the top-left and add the image to the scene
-    var pipe = this.add.sprite(CELL_WIDTH, CELL_WIDTH, 'pipe').setInteractive();
-    pipe.setScale(0.35); // resize the pipe to be the same height as a cell on the grid
-    this.input.setDraggable(pipe);
+    this.add.image(OFFSET, OFFSET+CELL_WIDTH, 'board').setOrigin(0,0); //set the origin of the image to the top-left and add the image to the scene
+
+    create_sprites(this, 10, ObjectType.REGULAR_PIPE);// creates 
  
     var previous_position; // previous position in pixels
     var previous_x; // previous x position in the 2D array
     var previous_y; // previous y position in the 2D array
- 
-    
     
     this.input.on('dragstart', function (pointer, gameObject, dropZone){
         //sets the starting grid block false
@@ -47,7 +57,10 @@ function create ()
         previous_position = [gameObject.x, gameObject.y];
         previous_x = (gameObject.x/CELL_WIDTH)-1;
         previous_y = (gameObject.y/CELL_WIDTH)-1;
-        grid[previous_x][previous_y] = false;
+        if (previous_y != 0){
+            grid[previous_y][previous_x] = null;
+        }
+        
     });
     
     this.input.on('drag', function(pointer, gameObject, dragX, dragY) {
@@ -59,7 +72,7 @@ function create ()
         var x = (gameObject.x/CELL_WIDTH)-1;
         var y = (gameObject.y/CELL_WIDTH)-1;
         
-        if (grid[x][y] === true){
+        if (grid[y][x] === true){
             //returns the pipe to its previous position
             gameObject.x = previous_position[0];
             gameObject.y = previous_position[1];
@@ -67,8 +80,10 @@ function create ()
             grid[previous_x][previous_y] = true;
         }else{
             //sets the new grid position as true (i.e. occupied)
-            grid[x][y] = true;
-        }  
+            grid[y][x] = true;
+        } 
+        
+        console.log(grid);
         
     });
  
@@ -84,14 +99,38 @@ function update(){
 }
  
 function create_grid(){
-    var grid = new Array(16);
+    var grid = new Array(17);
+    var grid_width = 16;
  
     for (var i = 0; i < grid.length; i++){
         grid[i] = new Array(16);
     }
+
+    // makes all elements null, makes first row elements true
+    for (var i = 0; i < grid.length; i++){
+        for (var j = 0; j < grid_width; j++){
+            if (i === 0){
+                grid[i][j] = true;
+            }else{
+                grid[i][j] = null;
+            }
+            
+        }
+    }
  
-    //NOTE: grid[x][y]
+    //NOTE: grid[y][x]
     return grid;
 }
 
 
+function create_sprites(context, number, type) {
+    //generates sprites givent the number of sprites needed and the type of sprite needed
+    if (type === ObjectType.REGULAR_PIPE){
+        for (var i = 0; i < number; i++){
+            var pipe = context.add.sprite(CELL_WIDTH, CELL_WIDTH, 'pipe').setInteractive();
+            pipe.setScale(0.35); // resize the pipe to be the same height as a cell on the grid
+            context.input.setDraggable(pipe);
+        }
+    }
+    
+}
