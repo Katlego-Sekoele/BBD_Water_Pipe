908434b4a017b0e5f2146eeabd38980cb0157919
Ṱharaga <55566621+Tharaga-K@users.noreply.github.com>
2021-07-28 11:06:19 +0200

    Adding water passage objects

---
+++ waterPassage.js
@@ -0,0 +1,18 @@
+const ObjectType = {
+	REGULAR_PIPE = 1;
+	
+}
+
+class gameEntity
+{
+	constructor(purity, kind)
+	{
+		this.purityLevel = -1; // -1 means there is no water in the object
+		this.kind
+	}
+	
+	function hasWater() {return purity === -1;}
+	function getWaterPurity() {return purity;}
+	function getKind() {return kind;}
+	
+}
