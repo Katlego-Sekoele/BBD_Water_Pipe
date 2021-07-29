8179451ebfc3bb69f2500fc98bb14f893ca9cc0a
Ṱharaga <55566621+Tharaga-K@users.noreply.github.com>
2021-07-29 09:20:35 +0200

    File to contain levels information

---
+++ levels.js
@@ -0,0 +1,19 @@
+LEVELS[
+	{
+		SOURCE:{y:,x:},
+		END:{y:,x:},
+		WATER_PURITY_LEVEL:3,
+		IMMOVABLES[
+			{type:, position:, quantity:}
+		]
+		MOVABLE[
+			{type:, position:, quantity:}
+		]
+	},
+	{
+	
+	},
+	{
+	
+	}
+]
