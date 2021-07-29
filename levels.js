9c0c6124a1da745ce081a42670c7a4b2188eb2fa
Ṱharaga <55566621+Tharaga-K@users.noreply.github.com>
2021-07-29 10:11:45 +0200

    Update levels.js

--- levels.js
+++ levels.js
@@ -1,19 +1,38 @@
-LEVELS[
-	{
-		SOURCE:{y:,x:},
-		END:{y:,x:},
-		WATER_PURITY_LEVEL:3,
-		IMMOVABLES[
-			{type:, position:, quantity:}
-		]
-		MOVABLE[
-			{type:, position:, quantity:}
+/*
+const ObjectType = {
+	SOURCE: 0,
+	PIPE: 1,
+	BENDLEFT: 2,
+	BENDRIGHT: 3,
+	CHECKPIPE: 4,
+	DOUBLEDUAL: 5,
+	DOUBLELEFT: 6,
+	DOUBLERIGHT: 7,
+	PURIFIER: 8,
+	FUNCTIONBLOCK: 9,
+	FUNCTIONCALL: 10,
+	END: 11
+}
+*/
+
+
+var LEVELS = {
+	numberOFLevels:1,
+	"1":{
+		SOURCE:{y:2,x:4},
+		END:{y:7,x:6},
+		WATER_PURITY_LEVEL:1,
+		IMMOVABLES:[],
+		MOVABLES:[
+			{type:ObjectType.PIPE,quantity:5},
+			{type:ObjectType.BENDRIGHT,quantity:10},
+			{type:ObjectType.BENDLEFT,quantity:10},
 		]
 	},
-	{
+	"2":{
 	
 	},
-	{
+	"3":{
 	
 	}
-]
+}
